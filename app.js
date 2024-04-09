490565c29fc691d6c2d41d9cc3ce7e6fabf79276
jamie-noel <jamie-ashton@live.nmit.ac.nz>
2024-04-09 14:29:51 +1200

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ output => [
     ["0", "1", "1", "1", "0"],
     ["0", "1", "$", "1", "0"],
     ["0", "1", "1", "1", "0"],
-    ["0", "0", "0", "0", "0"]
+    ["0", "1", "$", "1", "0"]
 ]
 */
 
@@ -31,9 +31,20 @@ if the y index is valid or exists i want to return arr[y][x] otherwise return nu
         // an array is constructed with elements represention the content of adjacent cells.
         //we are going to use checkAcess for each of he eight possible adjacent positions.
         return [
-
-        ]
+            checkAccess(y+1, x), // checks the cell directly bellow the current (y, x)
+            checkAccess(y+1, x+1), //checks diagonally bellow and to the right of the current cell.
+            checkAccess(y+1, x-1), //check diagonally bellow and to the left,
+            checkAccess(y, x+1), // check directly to the right 
+            checkAccess(y, x-1), //check directly to the left
+            checkAccess(y-1, x), // checks directly above.
+            checkAccess(y-1, x+1), // check th cell diagonally to the right.
+            checkAccess(y-1, x-1) // check the cell diagonally above to the left.
+            //NOTE - construct an array of values represents the content of the cells adjacents, 
+            //after the constructing of the array we will use filter method to keep those elements 
+            //that are mines ('$')    
+        ] .filer(adjCell => adjCell === "$").length.toString()
     }
+    return arr.map((r,y) => r.map((c,x) => c === "-" ? getMine(x,y) : c))
 }
 
 
@@ -50,3 +61,12 @@ function mineGrid() {
         console.log(mineGrid[i]);
     }
 }
+
+
+
+
+            // checkAccess(y+1, x), checkAccess(y+1, x+1),checkAccess(y+1, x-1),
+            // checkAccess(y-2, x ), checkAccess(y+2, x), checkAccess(y+2, x),
+            // checkAccess(y+3), checkAccess(), checkAccess(),
+            // checkAccess(y+4), checkAccess(), checkAccess(),
+            // checkAccess(y+5), checkAccess(), checkAccess(),
